a197c960c0b91b8cdd0fb0eddb95c0fcc062589e
fta <abiliomatute1@gmail.com>
2023-10-25 17:47:32 +0100

    Added default template

---
+++ user-script-template.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Lorem ipsum.
+// @namespace   https://github.com/ftaplayz/public-js-scripts
+// @match       https://www.example.com
+// @icon        https://www.example.com
+// @grant       none
+// @version     1.0
+// @author      ftaplayz
+// @description Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin placerat. 
+// ==/UserScript==
