8fce3c040e7d7bf794d43339e1e352942d4334c5
fta <abiliomatute1@gmail.com>
2023-11-25 02:23:35 +0000

    epic games capmonster

---
+++ capmonster.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        CapMonster Auto hCaptcha (Epic Games)
+// @namespace   https://github.com/ftaplayz/public-js-scripts
+// @match       *://*.epicgames.com/*
+// @icon        https://store.epicgames.com/
+// @grant       none
+// @version     1.0
+// @author      ftaplayz
+// @require     https://raw.githubusercontent.com/ftaplayz/public-js-scripts/main/cap.js
+// @run-at      document-end
+// @noframes
+// @description Auto complete hCaptcha for Epic Games Store
+// ==/UserScript==
+var c = new CaptchaSolver('API KEY');
+document.addEventListener('DOMNodeInserted', function(event){
+	if(c.hasHCaptcha())
+        c.solveHCaptcha();
+})
+
